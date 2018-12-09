529db3b2c148e77078ded2b89990d4e520b8bbd6
Royce Santo <roycesanto@yahoo.com>
2018-12-09 23:36:42 +0800

    Base files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health;
+    public int stagger;
+
+    public void SelectAttack()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Stagger()
+    {
+
+    }
+}
