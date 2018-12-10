1292965f7265f5a4c4bc505d1774fb835838234a
Royce Santo <roycesanto@yahoo.com>
2018-12-11 01:56:16 +0800

    Damage function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public int health;
-    
 
+    public void Damage(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
 }
