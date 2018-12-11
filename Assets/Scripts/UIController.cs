db361ce28b37d19a5545f8ee4fe254a7c823861d
Royce Santo <roycesanto@yahoo.com>
2018-12-11 23:03:52 +0800

    HP/Stagger Bar UI and Controller

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField] private Slider playerHPBar;
+    [SerializeField] private Slider enemyHPBar;
+    [SerializeField] private Slider enemyStaggerBar;
+
+    public void UpdatePlayerHPBar(float percentage)
+    {
+        playerHPBar.value = percentage;
+    }
+
+    public void UpdateEnemyHPBar(float percentage)
+    {
+        enemyHPBar.value = percentage;
+    }
+
+    public void UpdateEnemyStaggerBar(float percentage)
+    {
+        enemyStaggerBar.value = percentage;
+    }
+}
